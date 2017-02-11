178e3744819171ffe1abc777baeb03ce66fe1368
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-11 21:27:25 +0300

    no message

--- Settings.php
+++ Settings.php
@@ -3,13 +3,13 @@ namespace Dfe\TwitterTimeline;
 /** @method static Settings s() */
 final class Settings extends \Df\Config\Settings {
 	/** @return string */
-	public function html() {return $this->v('html');}
+	function html() {return $this->v('html');}
 
 	/** @return int */
-	public function limit() {return $this->i('limit');}
+	function limit() {return $this->i('limit');}
 
 	/** @return bool */
-	public function transparent() {return $this->b('transparent');}
+	function transparent() {return $this->b('transparent');}
 
 	/**
 	 * @override
